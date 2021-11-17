2d949dc437f5d368eab609b17e34a16771431628
Laith Alwani <laithalwani@gmail.com>
2021-11-17 11:26:35 -0500

    updated profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,42 @@
-import { useContext } from 'react'
-import UserContext from '../utils/UserContext';
-import { Avatar } from '@mui/material';
+import { useContext, useEffect } from "react";
+import UserContext from "../utils/UserContext";
+import { Avatar, Container, Grid } from "@mui/material";
+import moment from "moment"
 
 export default function Profile() {
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+
+  const getDate = dateCreated => {
+    // return (moment.unix(dateCreated/1000).format("MMM DD YYYY"));
+    return (moment.unix(dateCreated/1000, "YYYYMMDD").fromNow());
+  }
+
+  useEffect(() => {
+    
+  },[user])
   console.log(user);
   return (
     <div>
       {user && (
-        <>
-          <Avatar src={user.photoURL} alt={user.displayName} />
-          <h1>Hello, {user.displayName} !</h1>
-          <p>{user.email}</p>
-        </>
+        <Container sx={{marginTop:"2rem"}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={3} sx={{
+              display: "flex", justifyContent:"center", alignItems:"felxStart"
+            }}>
+              <Avatar
+                src={user.photoURL}
+                alt={user.displayName}
+                sx={{width:"150px", height:"150px", fontSize:"7rem" }}
+              />
+            </Grid>
+            <Grid item xs={12} md={9}>
+              <h1>{user.displayName}</h1>
+              <p>Email: {user.email}</p>
+              <p>Verified User: {user.emailVerified ? "yes": "no"}</p>
+              <p>Joined: {getDate(user.createdAt)}</p>
+            </Grid>
+          </Grid>
+        </Container>
       )}
     </div>
   );
